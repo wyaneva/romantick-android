50f0a7ac10e887a39156966dbfc3352eecb120bb
Vanya Yaneva <wyaneva@gmail.com>
2014-06-21 20:18:59 +0100

    First commit

---
+++ src/com/examples/romantick/MainActivity.java
@@ -0,0 +1,45 @@
+package com.examples.romantick;
+
+
+import com.example.romantick.R;
+import com.example.romantick.R.id;
+import com.example.romantick.R.layout;
+import com.example.romantick.R.menu;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+	public static String result = "";
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    public void sendMessage(View view)
+    {
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
+    }
+
+    //Constants
+    public final static String EXTRA_MESSAGE = "com.example.romantick.MESSAGE";
+}
